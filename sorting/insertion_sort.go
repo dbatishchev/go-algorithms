a1741c5cbe5e3a8a0894a43d506350ee4cdb1354
dbatishchev <dimitrybatishchev@gmail.com>
2017-04-01 11:50:48 +0300

    insertion sort added

--- sorting/insertion_sort.go
+++ sorting/insertion_sort.go
@@ -1,1 +1,17 @@
 package sorting
+
+import "go-algorithms/utils"
+
+func InsertionSort(s []int) []int {
+	if len(s) < 2 {
+		return s
+	}
+	for i := 1; i < len(s); i++ {
+		for j := i; j > 0; j-- {
+			if (s[j] < s[j - 1]) {
+				utils.Swap(s, j, j - 1)
+			}
+		}
+	}
+	return s
+}
--- sorting/insertion_sort_test.go
+++ sorting/insertion_sort_test.go
@@ -1,1 +1,22 @@
 package sorting
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestInsertionSort(t *testing.T) {
+	testlist := []int{5, 6, 4, 3, 9, 8, 7, 1, 2, 4, 3, 0}
+	testlist = InsertionSort(testlist)
+
+	for i := 0; i < len(testlist); i++ {
+		fmt.Println(testlist[i])
+		if (i == 0) {
+			continue
+		}
+		if testlist[i] < testlist[i - 1] {
+			t.Error("Invalid array sort")
+			break
+		}
+	}
+}
