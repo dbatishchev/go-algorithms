0d03a6061a533a3807814f124e844b89da78d3f1
dbatishchev <dimitrybatishchev@gmail.com>
2017-04-01 13:14:21 +0300

    add quick sort

--- sorting/quick_sort.go
+++ sorting/quick_sort.go
@@ -1,1 +1,35 @@
 package sorting
+
+import "math/rand"
+
+func QuickSort(s []int) []int {
+	if len(s) < 2 {
+		return s
+	}
+
+	pivot := s[rand.Intn(len(s))]
+
+	l := []int{}
+	m := []int{}
+	r := []int{}
+
+	for _, v := range s {
+		if (v < pivot) {
+			l = append(l, v)
+		}
+		if (v == pivot) {
+			m = append(m, v)
+		}
+		if (v > pivot) {
+			r = append(r, v)
+		}
+	}
+
+	l = QuickSort(l)
+	r = QuickSort(r)
+
+	result := append(l, m...)
+	result = append(result, r...)
+
+	return result
+}
